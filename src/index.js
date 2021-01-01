cb4ba555429185cd4de8a4007e3cd3dc424b6bd3
AnkitKumar117 <ankipandel1234@gmail.com>
2021-01-01 13:58:19 +0530

    repo Intialized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+const App = ()=>{
+    return  (
+        <div className="ui container comments">
+            <CommentDetail author="p1" />
+            <CommentDetail author="p2" />
+            <CommentDetail author="p3" />
+            <CommentDetail author="p4" />
+            
+        </div>
+    );
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/> , document.querySelector('#root'));
